686a98356439523dd1b144a3e7f3d02007b6083a
Viliam Simko <viliam.simko@gmail.com>
2015-09-15 16:45:39 +0200

    Working RF transmitter + receiver that sends temperature+humidity from DHT22 and temperature from a thermistor

--- src/sensor/DHT22-WorkingPowerOnPin5/DHT22-WorkingPowerOnPin5.ino
+++ src/sensor/DHT22-WorkingPowerOnPin5/DHT22-WorkingPowerOnPin5.ino
@@ -1,23 +1,19 @@
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 
+#include "VirtualWire.h"
 #include "DHT.h"
-#include <math.h>
 
 #define POWERPIN 5 // power for DHT sensor
 #define DHTPIN 6 // data pin for DHT sensor
+#define RFTXPIN 12 // data pin for RF Transmission
 
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE);
 
-double Thermister(int RawADC) {
-  double Temp;
-  // See http://en.wikipedia.org/wiki/Thermistor for explanation of formula
-  Temp = log(((10240000 / RawADC) - 10000));
-  Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
-  Temp = Temp - 273.15;           // Convert Kelvin to Celcius
-  return Temp;
-}
+char* sensor_id = "Sensor1";
+const int transmit_pin = 12;
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
@@ -27,10 +23,18 @@ void setup() {
   Serial.println(POWERPIN);
   pinMode(POWERPIN, OUTPUT);
 
+  vw_set_tx_pin(transmit_pin);
+  vw_setup(2000); // Bits per sec
+
   dht.begin();
+
+  Serial.println("Setting up RF TX module");
+//  vw_set_tx_pin(RFTXPIN);
+//  vw_setup(2000); // Bits per sec
 }
 
 void loop() {
+
   digitalWrite(POWERPIN, LOW);
 
   // Wait a few seconds between measurements.
@@ -68,4 +72,33 @@ void loop() {
   Serial.print(hic);
   Serial.print(" °C");
   Serial.println();
+
+  char msg[32];
+  sprintf(msg, "%s %d.%d %d.%d %d.%d",
+    sensor_id,
+    (int) temp, frac2(temp),
+    (int) t, frac2(t),
+    (int) h, frac2(h)
+  );
+  vw_send((uint8_t *)msg, strlen(msg));
+  vw_wait_tx(); // Wait until the whole message is gone
+  Serial.println(msg);
 }
+
+int frac2(double x) {
+  return abs((x - (int) x) * 100);
+}
+
+/**
+ * Reads the voltage from a 10K thermistor and transforms
+ * it into temparature (celsius).
+ */
+double Thermister(int RawADC) {
+  double Temp;
+  // See http://en.wikipedia.org/wiki/Thermistor for explanation of formula
+  Temp = log(((10240000 / RawADC) - 10000));
+  Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
+  Temp = Temp - 273.15;           // Convert Kelvin to Celcius
+  return Temp;
+}
+
--- src/sensor/DemoTransmitter/DemoTransmitter.ino
+++ src/sensor/DemoTransmitter/DemoTransmitter.ino
@@ -10,28 +10,51 @@
 
 #include <VirtualWire.h>
 
+#define POWERPIN 5 // power for DHT sensor
+
+
 const int led_pin = 13;
 const int transmit_pin = 12;
 
 void setup()
 {
+  pinMode(POWERPIN, OUTPUT);
+  digitalWrite(POWERPIN, HIGH);
+  
+
     // Initialise the IO and ISR
     vw_set_tx_pin(transmit_pin);
     vw_setup(2000); // Bits per sec
     pinMode(led_pin, OUTPUT);
-}
 
-byte count = 0;
+  Serial.begin(9600);
+  Serial.println("RF transmitter demo");
+    
+}
 
 void loop()
 {
-  char msg[7] = {'h','e','l','l','o',' ','#'};
+  double temp = Thermistor(analogRead(0));
 
-  msg[6] = 'A' + count;
+  char msg[32];
+  int dtemp = temp;
+  int ftemp = abs( (temp - dtemp) * 100);
+  sprintf(msg, "T=%d.%d", dtemp, ftemp );
+  Serial.println(msg);
+
   digitalWrite(led_pin, HIGH); // Flash a light to show transmitting
-  vw_send((uint8_t *)msg, 7);
+  vw_send((uint8_t *)msg, strlen(msg));
   vw_wait_tx(); // Wait until the whole message is gone
   digitalWrite(led_pin, LOW);
   delay(1000);
-  count = (count + 1) % 24;
 }
+
+double Thermistor(int RawADC) {
+  double Temp;
+  // See http://en.wikipedia.org/wiki/Thermistor for explanation of formula
+  Temp = log(((10240000 / RawADC) - 10000));
+  Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
+  Temp = Temp - 273.15;           // Convert Kelvin to Celcius
+  return Temp;
+}
+
