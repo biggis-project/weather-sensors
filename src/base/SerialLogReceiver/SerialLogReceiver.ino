64be3cdedc86b98b473465af78e409bc975a4050
Viliam Simko <viliam.simko@gmail.com>
2015-09-16 18:08:00 +0200

    refactored code of simple receiver

--- src/base/SerialLogReceiver/SerialLogReceiver.ino
+++ src/base/SerialLogReceiver/SerialLogReceiver.ino
@@ -1,21 +1,24 @@
 #include <VirtualWire.h>
 
-const int led_pin = 13;    // PIN 13 has an integrated LED on Arduino Nano
-const int receive_pin = 9;
+#define RF_RX_PIN 2
+#define LED_PIN 13
+#define RF_POWER_PIN 4
+#define RF_BITRATE 2000
 
 void setup()
 {
-    delay(1000);
     Serial.begin(9600);	// Debugging only
     Serial.println("Setting up the receiver");
-    pinMode(led_pin, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 
     // Initialise the IO and ISR
-    vw_set_rx_pin(receive_pin);
-    vw_setup(2000);	 // Bits per sec
+    pinMode(RF_POWER_PIN, OUTPUT); // power for the RF module
+    digitalWrite(RF_POWER_PIN, HIGH);
+    
+    vw_set_rx_pin(RF_RX_PIN);
+    vw_setup(RF_BITRATE);
 
-    vw_rx_start();       // Start the receiver PLL running
-
+    vw_rx_start(); // Start the receiver PLL running
 }
 
 void loop()
@@ -23,21 +26,21 @@ void loop()
     uint8_t buf[VW_MAX_MESSAGE_LEN];
     uint8_t buflen = sizeof(buf);
 
-    if (vw_get_message(buf, &buflen)) // Non-blocking
-    {
-      	int i;
-      
-        digitalWrite(led_pin, HIGH); // Flash a light to show received good message
-      	// Message with a good checksum received, dump it.
-      	Serial.print("Got: ");
-
+    // Non-blocking
+    if (vw_get_message(buf, &buflen)) {
+      digitalWrite(LED_PIN, HIGH); // Flash a light to show received good message
+        // Message with a good checksum received, dump it.
+        Serial.print("Got: ");
+        
         char cbuf[buflen+1];
         cbuf[buflen] = '\0';
-      	for (i = 0; i < buflen; i++) {
+        for (int i = 0; i<buflen; i++) {
           cbuf[i] = (char) buf[i];
-      	}
-        Serial.print(cbuf);
-      	Serial.println();
-        digitalWrite(led_pin, LOW);
+        }
+        
+        Serial.println(cbuf);
+        delay(500);
+        
+      digitalWrite(LED_PIN, LOW);
     }
 }
